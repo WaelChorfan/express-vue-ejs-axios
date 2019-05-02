4c2b79d0d8c00ed33cb47806e9431c094b323dc7
WaelChorfan <chorfanwael@gmail.com>
2019-05-02 20:58:53 +0200

    first commit

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('posts',{title:"tasks from express server"});
+  res.render('posts',{title:"posts from express server"});
 });
 
 module.exports = router;
